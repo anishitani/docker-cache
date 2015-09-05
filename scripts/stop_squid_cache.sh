463baa2d5fa4accfe683d774db231988b3b180ab
André Nishitani <atoshio25@gmail.com>
2015-09-05 20:23:30 -0300

    fix cache not applied

--- scripts/stop_squid_cache.sh
+++ scripts/stop_squid_cache.sh
@@ -1,3 +1,4 @@
-rm /etc/apt/apt.conf.d/30proxy
-sed -i '/use_proxy=/d' /etc/wgetrc
-sed -i '/http_proxy=/d' /etc/wgetrc
+rm /etc/apt/apt.conf.d/30proxy \
+|| sed -i '/use_proxy=/d' /etc/wgetrc \
+|| sed -i '/http_proxy=/d' /etc/wgetrc \
+|| true
